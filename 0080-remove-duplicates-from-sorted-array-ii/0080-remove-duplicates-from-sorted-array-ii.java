2826de5f359290f11eff0983d41a09b1986230d9
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2023-08-23 21:26:01 +0900

    Time: 0 ms (100.00%), Space: 43.8 MB (52.05%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,15 +1,34 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+        
+//         int idx = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i > nums.length - 3 || nums[i] != nums[i + 2]) {
+//                 nums[idx] = nums[i];
+//                 idx += 1;
+//             }
+//         }
+        
+//         return idx;
+        
+//     }
+// }
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-        int idx = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > nums.length - 3 || nums[i] != nums[i + 2]) {
-                nums[idx] = nums[i];
-                idx += 1;
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
+        int index = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
             }
         }
-        
-        return idx;
-        
+
+        return index;
     }
 }
