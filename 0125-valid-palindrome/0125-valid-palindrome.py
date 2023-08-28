2f8023d260379a19fb2923b2a7036a6c073b8764
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2023-08-28 13:21:09 +0900

    Time: 48 ms (82.06%), Space: 18.6 MB (13.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,13 +7,15 @@ class Solution:
         regex = r'[a-zA-Z0-9]'
         result = re.findall(regex, s)
         
-        # 문자중 모든 대문자를 소문자로 변환한다.
+        # 문자중 모든 대문자를 소문자로 변환한다. # O(n)
         for i in range(len(result)):
             elem = result[i]
             if elem.isupper():
                 result[i] = elem.lower()
 
-        # 팬린드롬인지 확인한다.
-        if result == result[::-1]:
-            return True
-        return False
+        # 팬린드롬인지 확인한다. # O(n)
+        n = len(result)
+        for i in range(n//2):
+            if result[i] != result[n-1-i]:
+                return False
+        return True
