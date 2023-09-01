51f36703907b2f433bd9741b61db4b7de940d807
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2023-09-01 17:25:16 +0900

    Time: 408 ms (76.23%), Space: 17.6 MB (23.87%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        max_idx = 0 # 최대로 이동가능한 거리
+        
+        for i in range(n):
+            if i > max_idx:
+                continue
+            
+            if i <= max_idx and i + nums[i] >= n - 1:
+                return True
+            
+            max_idx = max(max_idx, i + nums[i])
+            
+        return False
