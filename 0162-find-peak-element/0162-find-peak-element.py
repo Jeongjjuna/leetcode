a8663e3faba8686858dc5a7a35d2d3dde74206c9
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2023-09-05 02:03:37 +0900

    Time: 40 ms (98.77%), Space: 16.5 MB (22.66%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,19 +1,29 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
+        INF = sys.maxsize
+        
         # 배열의 요소가 1개일 경우는 그 값이 Peak
         if len(nums) == 1:
             return 0
         
-        INF = sys.maxsize
-        for i in range(len(nums)):
-            if i == 0:
-                if -INF < nums[i]  and nums[i + 1] < nums[i]:
-                    return i
-                
-            if i == len(nums) - 1:
-                if nums[i - 1] < nums[i]  and -INF < nums[i]:
-                     return i
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             
-            if nums[i - 1] < nums[i]  and nums[i + 1] < nums[i]:
-                return i
+            b = nums[mid]
+            if mid == len(nums) - 1:
+                a = nums[mid - 1]
+                c = -INF
+            elif mid == 0:
+                a = -INF
+                c = nums[mid + 1]
+            else:
+                a, c = nums[mid - 1], nums[mid + 1]
+            
+            if b < c:
+                left = mid + 1
+            elif a > b:
+                right = mid - 1
+            else:
+                return mid
