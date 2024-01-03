fdc212eb78323d9905353fb3493f6c4a5c47cbd0
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-03 11:41:00 +0900

    Time: 81 ms (97.96%), Space: 20.5 MB (50.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    # 접근1. 정렬하기
+    def sol_1(self, strs):
+        answer = []
+        
+        d = dict()
+        for elem in strs:
+            key = "".join(sorted(elem))
+            
+            if not key in d:
+                d[key] = []          
+            d[key].append(elem)
+    
+ 
+        for key in d:
+            answer.append(d[key])
+    
+        return answer
+    
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        return self.sol_1(strs)
