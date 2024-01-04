2088dc708ceb3234e51eea336a9ee6531164475b
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-04 19:23:43 +0900

    Time: 32 ms (89.66%), Space: 17.3 MB (13.75%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,12 +1,29 @@
 class Solution:
-    # 접근 1. 구현(완탐)
+    # 접근1. 구현(완탐) -> O(n**2)
     def sol_1(self, jewels, stones):
         cnt = 0
-        for elem in stones:
-            if elem in jewels:
+        for elem in stones: # O(n)
+            if elem in jewels: # O(n)
                cnt += 1
         return cnt
-        
+
+    
+    # 접근2. 해시 -> O(n) + O(n) + O(1) = O(n)
+    def sol_2(self, jewels, stones):
+        # 사전에 값 넣기
+        d = dict()
+        for elem in jewels: # O(n)
+            d[elem] = 1
+            
+        # 사전에 존재하는지 확인하기
+        cnt = 0
+        for elem in stones: # O(n)
+            if elem in d: # O(1)
+                cnt += 1
+        return cnt
+    
+    
+    
     
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        return self.sol_1(jewels, stones);
+        return self.sol_2(jewels, stones);
