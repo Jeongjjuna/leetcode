bc6c2e64937693a2597c98b83e89c7c1c7c4d3e9
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-06 08:38:03 +0900

    Time: 46 ms (97.20%), Space: 17.5 MB (5.76%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -1,6 +1,10 @@
+import heapq
+
 class Solution:
     
-    # 접근1. 완전탐색 -> O(n**2)
+    # 접근1. 완전탐색
+    # 시간복잡도 : O(n**2)
+    # 실행시간 : 993ms
     def sol_1(self, nums):        
         ans = 0
         n = len(nums)
@@ -13,11 +17,28 @@ class Solution:
                 
         return ans
     
-    # 접근2. 정렬 -> O(nlogn)
+    
+    # 접근2. 정렬
+    # 시간복잡도 : O(nlogn)
+    # 실행시간 : 53ms
     def sol_2(self, nums):
         nums.sort(reverse = True)
         return (nums[0] - 1) * (nums[1] - 1)
     
     
+    # 접근3. 힙
+    # 시간복잡도 : O(nlogn)
+    # 실행시간 : 
+    def sol_3(self, nums):
+        hq = []
+        for elem in nums:
+            heapq.heappush(hq, -elem)
+        
+        max_val_1 = -heapq.heappop(hq)
+        max_val_2 = -heapq.heappop(hq)
+
+        return (max_val_1 - 1) * (max_val_2 - 1)
+    
+    
     def maxProduct(self, nums: List[int]) -> int:
-        return self.sol_2(nums)
+        return self.sol_3(nums)
