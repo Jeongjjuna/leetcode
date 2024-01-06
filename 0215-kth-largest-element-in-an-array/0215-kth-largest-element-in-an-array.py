f064628aea7e34b96bb1a1ae523b9791436c39c2
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-06 09:08:20 +0900

    Time: 533 ms (58.36%), Space: 30.4 MB (32.58%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,5 @@
 import heapq
+from collections import defaultdict
 
 # 배열에서 k번째로 큰 요소
 # n = 100000
@@ -9,7 +10,7 @@ class Solution:
  
     # 접근1. 힙 -> 정렬사용해도 똑같음
     # 시간복잡도 : O(nlogn)
-    # 실행시간 : ?
+    # 실행시간 : 515ms
     def sol_1(self, nums, k):
         hq = []
         for elem in nums:
@@ -21,5 +22,30 @@ class Solution:
         return val
     
     
+    
+    # nums[i]가 최대 10000
+    # 카운팅정렬해볼까?
+    def sol_2(self, nums, k):
+        d = defaultdict(int)
+        
+        max_val = 0
+        min_val = 10001
+        for elem in nums:
+            d[elem] += 1
+            max_val = max(max_val, elem)
+            min_val = min(min_val, elem)
+        
+        temp = k
+        for i in range(max_val, min_val - 1, -1):
+            if i not in d:
+                continue
+                
+            if temp <= d[i]:
+                return i
+            
+            temp -= d[i]
+     
+    
+    
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.sol_1(nums, k)
+        return self.sol_2(nums, k) 
