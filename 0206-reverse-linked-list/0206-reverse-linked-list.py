69151ab3b0cd37351b60e35cb0a71bc4c931a657
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-07 20:19:09 +0900

    Time: 43 ms (51.04%), Space: 18.6 MB (28.87%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,9 +3,15 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+from collections import deque
+
+
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+    
+    # 접근1. 연결리스트를 앞에서부터 탐색하며 거꾸로 연결하기
+    # 시간복잡도 : O(n)
+    def sol_1(self, head):
         n = head
         prev = None
         
@@ -14,5 +20,29 @@ class Solution:
             n.next = prev
             prev = n
             n = next
-        
+            
         return prev
+    
+    
+    # 접근2. deque로 변환하기 O(n)
+    def sol_2(self, head):
+        if head == None:
+            return head
+        
+        dq = deque()
+        # dq에 순차적으로 집어넣기
+        node = head
+        while node != None:
+            dq.append(node)
+            node = node.next
+        
+        # 뒤에서부터 재연결해주기
+        for i in range(len(dq) - 1, 0, -1):
+            dq[i].next = dq[i - 1]
+        dq[0].next = None
+        
+        return dq[len(dq) - 1]
+        
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.sol_2(head)
