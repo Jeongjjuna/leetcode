17823d2afddf6d2365fb356121893c89f59923e4
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-10 12:17:07 +0900

    Time: 32 ms (88.72%), Space: 17.3 MB (29.33%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,8 +2,23 @@ from collections import deque
 
 class Solution:
     
-    def __init__(self):
-        self.phone = {
+    # 접근1. dfs
+    def sol_1(self, digits):
+        
+        def dfs(digits, dep, curr_str): # s = ["2", "3"], pre = ["a, d"]
+            if dep == len(digits):
+                ans.append("".join(curr_str))
+                return
+
+            for nv in phone[digits[dep]]:
+                curr_str.append(nv)
+                dfs(digits, dep + 1, curr_str)
+                curr_str.pop()
+            
+        
+        # 변수 입력 및 선언
+        ans = []
+        phone = {
             "2":["a", "b", "c"],
             "3":["d", "e", "f"],
             "4":["g", "h", "i"],
@@ -13,28 +28,21 @@ class Solution:
             "8":["t", "u", "v"],
             "9":["w", "x", "y", "z"]
         }
-    
-    def dfs(self, s, pre, ans): # s = ["2", "3"]
-        if len(s) == 0:
-            ans.append("".join(pre))
-            return
-        
-        val = s.popleft()
-        for nv in self.phone[val]:
-            pre.append(nv)
-            self.dfs(s, pre, ans)
-            pre.pop()
-            
-        s.appendleft(val)
-
-        
-    # 접근1. dfs
-    def letterCombinations(self, digits: str) -> List[str]:
-
-        ans = []
+       
+        # 입력값이 빈 문자열일 경우
         if digits == "":
             return ans
-        else:
-            self.dfs(deque(list(digits)), [], ans)
         
+        dfs(digits, 0, [])
         return ans
+
+    
+    def letterCombinations(self, digits: str) -> List[str]:
+        return self.sol_1(digits)
+
+    
+    
+    
+    
+    
+    
