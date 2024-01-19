3cb54388cda2e92ddd1a5567fb1115873d3fb217
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-19 09:58:23 +0900

    Time: 98 ms (96.91%), Space: 18.7 MB (5.33%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,7 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    
+    # 접근1. 이진 탐색 O(logN)
+    def sol_1(self, numbers, target):
         p1, p2 = 0, len(numbers) - 1
         
         while p1 < p2:
@@ -15,3 +17,20 @@ class Solution:
                 break
                 
         return [p1 + 1, p2 + 1]
+    
+    
+    # 접근2. 해시 O(n)
+    def sol_2(self, numbers, target):
+        d = dict()
+        
+        for i, elem in enumerate(numbers):
+            if target - elem in d:
+                return [d[target - elem], i + 1]
+            
+            d[elem] = i + 1
+                
+        
+    
+    # O(NlogN)이하로 해결해야함
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        return self.sol_2(numbers, target)
