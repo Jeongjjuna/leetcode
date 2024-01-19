4b3c24ee43478477d895de8b532726e77928c33c
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-01-19 10:37:24 +0900

    Time: 157 ms (14.28%), Space: 18.6 MB (5.33%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,6 @@
 class Solution:
     
-    # 접근1. 이진 탐색 O(logN)
+    # 접근1. 투포인터 O(N)
     def sol_1(self, numbers, target):
         p1, p2 = 0, len(numbers) - 1
         
@@ -19,7 +19,7 @@ class Solution:
         return [p1 + 1, p2 + 1]
     
     
-    # 접근2. 해시 O(n)
+    # 접근2. 해시 O(N)
     def sol_2(self, numbers, target):
         d = dict()
         
@@ -28,9 +28,34 @@ class Solution:
                 return [d[target - elem], i + 1]
             
             d[elem] = i + 1
-                
+        
+        
+    # 접근3. 이진탐색 O(NlogN)
+    def sol_3(self, numbers, target):
+        
+        for i, elem in enumerate(numbers):
+            
+            left, right = i + 1, len(numbers) - 1
+            expected = target - elem
+            idx = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+                if numbers[mid] == expected: # 찾았다면 해당 index를 반환합니다.
+                    idx = mid
+                    break
+
+                if numbers[mid] > expected:
+                    right = mid - 1  
+                else:           
+                    left = mid + 1
+            
+            if idx == -1:
+                continue
+            
+            return [i + 1, idx + 1]
         
     
     # O(NlogN)이하로 해결해야함
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        return self.sol_2(numbers, target)
+        return self.sol_3(numbers, target)
